7bed32dcfb73bf6908cd2bc1cb9c777443f3bfba
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-03-23 03:52:34 -0400

    delete some information

--- cat/src/assets/components/Mainpage.jsx
+++ cat/src/assets/components/Mainpage.jsx
@@ -1,43 +1,43 @@
-import './Mainpage.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-//import catOption  from './catOption';
+// import './Mainpage.css';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// //import catOption  from './catOption';
 
-const Mainpage = () => {
+// const Mainpage = () => {
 
-  const [catsData, setCatsData] = useState([null]);
+//   const [catsData, setCatsData] = useState([null]);
 
-  const fetchData = () => {
-    axios.get('https://api.thecatapi.com/v1/images/search?api_key=live_KRlJiuUrksU0Hpq6JmbLSsA9SKpQwUyzs6dEFIkOqTp4ycRXZ3KvGEqnfskWWr9v')
-      .then((response) => {
-        setCatsData(response.data[0]);
-      })
-      .catch((error) => {
-        console.error('Error fetching data: ', error);
-      });
-  }
+//   const fetchData = () => {
+//     axios.get('https://api.thecatapi.com/v1/images/search?api_key=')
+//       .then((response) => {
+//         setCatsData(response.data[0]);
+//       })
+//       .catch((error) => {
+//         console.error('Error fetching data: ', error);
+//       });
+//   }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-console.log(catsData);
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+// console.log(catsData);
 
-  return (
-    <div className="mainpage">
-      <h1>Welcome to a Cat World!</h1>
-      <p>🐱*😸*😹*😻*😾*🐱*😸*😹*😻</p>
-      <button onClick={fetchData}>Get a new cat</button>
-      <p></p>
-      <div className = "cat-info">
-        <button>Id: {catsData.id}</button>
-        <button>Url: {catsData.url}</button>
-        <button>Width: {catsData.width}</button>
-        <button>Height: {catsData.height}</button>
-      </div>
-      <p></p>
-      <div className = "image-container"><img src={catsData.url} alt="Cat" /></div>
+//   return (
+//     <div className="mainpage">
+//       <h1>Welcome to a Cat World!</h1>
+//       <p>🐱*😸*😹*😻*😾*🐱*😸*😹*😻</p>
+//       <button onClick={fetchData}>Get a new cat</button>
+//       <p></p>
+//       <div className = "cat-info">
+//         <button>Id: {catsData.id}</button>
+//         <button>Url: {catsData.url}</button>
+//         <button>Width: {catsData.width}</button>
+//         <button>Height: {catsData.height}</button>
+//       </div>
+//       <p></p>
+//       <div className = "image-container"><img src={catsData.url} alt="Cat" /></div>
       
-    </div>
-  )
-}
-export default Mainpage;
+//     </div>
+//   )
+// }
+// export default Mainpage;
